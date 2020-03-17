c099f646df883241009047774e6fe21512c0592d
HunterWilkins <wilkins.hunter@gmail.com>
2020-03-17 10:12:19 -0500

    Changed publicPath entirely to just /images/thumbnails

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.get("/api/artwork", function(req, res) {
     console.log("Routing =/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=");
-    const publicPath = path.join(__dirname, "/images/thumbnails");
+    const publicPath = "/images/thumbnails";
     fs.readdirSync(publicPath, function(err, files) {
         if (err) {
             console.log(err);
