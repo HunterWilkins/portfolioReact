39834c03e9406d8375040af41392576f2d81128e
HunterWilkins <wilkins.hunter@gmail.com>
2020-03-17 09:36:22 -0500

    added /client/ to previous commit's path.join

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "build")));
+    app.use(express.static(path.join(__dirname, "/client/build")));
 }
 
 
